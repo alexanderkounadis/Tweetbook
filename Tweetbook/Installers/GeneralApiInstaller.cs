d2778d5ff160f644e8c6f5c48320b541419056c9
akounadis <akounadis@novibet.com>
2021-08-04 19:08:14 +0300

    Add project files.

---
+++ Tweetbook/Installers/GeneralApiInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tweetbook.Installers
+{
+    public class GeneralApiInstaller : IInstaller
+    {
+        public void InstallServices(IConfiguration config, IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+        }
+    }
+}
